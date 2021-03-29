224969e64c648fb9c0adcbd2a5c8df21a7284c9d
ShinjiTheRealG <81447184+ShinjiTheRealG@users.noreply.github.com>
2021-03-29 14:24:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2212,27 +2212,27 @@ var prompts = [{
 // This array stores all of the possible values and the weight associated with the value. 
 // The stronger agreeance/disagreeance, the higher the weight on the user's answer to the prompt.
 var prompt_values = [{
-    value: 'Strongly Agree',
+    value: 'Agree',
     class: 'btn-default btn-strongly-agree',
     weight: 2
   },
   {
-    value: 'Agree',
+    value: 'Somewhat Agree',
     class: 'btn-default btn-agree',
     weight: 1,
   },
   {
-    value: 'Neutral',
+    value: 'Neutral/Unsure',
     class: 'btn-default',
     weight: 0
   },
   {
-    value: 'Disagree',
+    value: 'Somewhat Disagree',
     class: 'btn-default btn-disagree',
     weight: -1
   },
   {
-    value: 'Strongly Disagree',
+    value: 'Disagree',
     class: 'btn-default btn-strongly-disagree',
     weight: -2
   }
